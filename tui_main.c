f311eddbd4c64a10f7bc2f2cb12a96a1640b3f37
boorch <burctuncer@gmail.com>
2024-03-14 02:01:11 +0000

    Update tui_main.c

--- tui_main.c
+++ tui_main.c
@@ -89,7 +89,9 @@ static Glyph_class glyph_class_of(Glyph glyph) {
   case '=':
   case '%':
   case '?':
+  case '|':
   case '$':
+  case '&':
     return Glyph_class_lowercase;
   case '*':
     return Glyph_class_bang;
